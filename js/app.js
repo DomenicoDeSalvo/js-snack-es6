f2ddf57d01785c54719ceb427d6ccee1bed25787
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-21 13:02:47 +0100

    Exercise 4, added array with just fouls and names.

--- js/app.js
+++ js/app.js
@@ -1,75 +1,75 @@
-// //ESERCIZIO 1
+//ESERCIZIO 1
 
-// //Array contente gli invitati in ordine di posto.
-// const tavoloVip = [ 
-//     'Brad Pitt', 
-//     'Johnny Depp',
-//     'Lady Gaga',
-//     'Cristiano Ronaldo',
-//     'Georgina Rodriguez',
-//     'Chiara Ferragni',
-//     'Fedez',
-//     'George Clooney',
-//     'Amal Clooney',
-//     'Maneskin'
-// ]; //Array
+//Array contente gli invitati in ordine di posto.
+const tavoloVip = [ 
+    'Brad Pitt', 
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+]; //Array
 
-// //Nuovo Array che conterrà la lista nel formato corretto.
+//Nuovo Array che conterrà la lista nel formato corretto.
 
-// const listaCorretta = tavoloVip.map((element, i)=>{
-//     return {
-//         Tavolo: 'Tavolo VIP',
-//         Nome: element,
-//         Posto: i + 1
-//     }
-// })
+const listaCorretta = tavoloVip.map((element, i)=>{
+    return {
+        Tavolo: 'Tavolo VIP',
+        Nome: element,
+        Posto: i + 1
+    }
+})
 
-// console.log(listaCorretta)
+console.log(listaCorretta)
 
-// //
+//
 
-// //ESERCIZIO 2
+//ESERCIZIO 2
 
-// //Array contenente i profili degli studenti.
-// const studentsList = [];//Array
+//Array contenente i profili degli studenti.
+const studentsList = [];//Array
 
-// //Creazione dei profili degli studenti i quali verranno poi inseriti tramite push nella lista.
-// const marcoDellaRovere = studentProfile(213, 'Marco della Rovere', 78);//Object.
-// studentsList.push(marcoDellaRovere);
-// const paolaCortellessa = studentProfile(110, 'Paola Cortellessa', 96);//Object.
-// studentsList.push(paolaCortellessa);
-// const andreaMantegna = studentProfile(250, 'Andrea Mantegna', 48);//Object.
-// studentsList.push(andreaMantegna);
-// const gaiaBorromini = studentProfile(145, 'Gaia Borromini', 74);//Object.
-// studentsList.push(gaiaBorromini);
-// const luigiGrimaldello = studentProfile(196, 'Luigi Grimaldello', 68);//Object.
-// studentsList.push(luigiGrimaldello);
-// const pieroDellaFrancesca = studentProfile(102, 'Piero della Francesca', 50);//Object.
-// studentsList.push(pieroDellaFrancesca);
-// const francescaDaPolenta = studentProfile(120, 'Francesca da Polenta', 84);//Object.
-// studentsList.push(francescaDaPolenta);
+//Creazione dei profili degli studenti i quali verranno poi inseriti tramite push nella lista.
+const marcoDellaRovere = studentProfile(213, 'Marco della Rovere', 78);//Object.
+studentsList.push(marcoDellaRovere);
+const paolaCortellessa = studentProfile(110, 'Paola Cortellessa', 96);//Object.
+studentsList.push(paolaCortellessa);
+const andreaMantegna = studentProfile(250, 'Andrea Mantegna', 48);//Object.
+studentsList.push(andreaMantegna);
+const gaiaBorromini = studentProfile(145, 'Gaia Borromini', 74);//Object.
+studentsList.push(gaiaBorromini);
+const luigiGrimaldello = studentProfile(196, 'Luigi Grimaldello', 68);//Object.
+studentsList.push(luigiGrimaldello);
+const pieroDellaFrancesca = studentProfile(102, 'Piero della Francesca', 50);//Object.
+studentsList.push(pieroDellaFrancesca);
+const francescaDaPolenta = studentProfile(120, 'Francesca da Polenta', 84);//Object.
+studentsList.push(francescaDaPolenta);
 
-// //Bisogna creare tre liste: una con i nomi di tutti in maiuscolo, una con gli studenti con un totale dei voti superiore al 70, una con quelli che oltre ad avete un totale dei voti superiore al 70 abbianco anche l`ID superiore a 120.
-// const upperCaseNames = studentsList.map((element)=>{
-//     return {
-//         ID: element.ID,
-//         Nome: element.Nome.toUpperCase(),
-//         Voti: element.Voti,
-//     }
-// })
-// const highGradesList = upperCaseNames.filter((element) => element.Voti > 70);
-// const highGradesAndIdList = upperCaseNames.filter((element) => element.Voti > 70 && element.ID > 120);
-// console.log(upperCaseNames, highGradesList, highGradesAndIdList);
+//Bisogna creare tre liste: una con i nomi di tutti in maiuscolo, una con gli studenti con un totale dei voti superiore al 70, una con quelli che oltre ad avete un totale dei voti superiore al 70 abbianco anche l`ID superiore a 120.
+const upperCaseNames = studentsList.map((element)=>{
+    return {
+        ID: element.ID,
+        Nome: element.Nome.toUpperCase(),
+        Voti: element.Voti,
+    }
+})
+const highGradesList = upperCaseNames.filter((element) => element.Voti > 70);
+const highGradesAndIdList = upperCaseNames.filter((element) => element.Voti > 70 && element.ID > 120);
+console.log(upperCaseNames, highGradesList, highGradesAndIdList);
 
-// //Funzione da utilizzare per creare gli oggetti contenenti i dati degli studenti.
-// function studentProfile(studentId, studentName, studentGrade){
-//     const student = {
-//         ID: parseInt(studentId), //Number.
-//         Nome: studentName,//String.
-//         Voti: parseInt(studentGrade),//Number.
-//     };
-//     return student; //Object.
-// }
+//Funzione da utilizzare per creare gli oggetti contenenti i dati degli studenti.
+function studentProfile(studentId, studentName, studentGrade){
+    const student = {
+        ID: parseInt(studentId), //Number.
+        Nome: studentName,//String.
+        Voti: parseInt(studentGrade),//Number.
+    };
+    return student; //Object.
+}
 
 
 //ESERCIZIO 3
@@ -155,6 +155,17 @@ calabroLeague.forEach((element, i)=>{
     element.drawnFouls = randomNumber(80, 120);//Number
 
 });//array.
+
+//Creazione nuovo array in cui sono presenti solo i nomi delle squadre e i falli subiti.
+const teamsDrawnFouls = calabroLeague.map((element)=>{
+    const {team, drawnFouls} = element
+    return {
+        team,
+        drawnFouls
+    }
+});//Array.
+
+console.log(teamsDrawnFouls)
 
 //Funzione per generare un numero casuale.
 function randomNumber(min, max) {
